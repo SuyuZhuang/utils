f7dfea2c5dd9fa8e4588c8e91290e162d2018f10
Suyu <zhuangsuyu1993@163.com>
2017-11-23 11:30:32 +0800

    Update user_contacter.sql

--- user_contacter.sql
+++ user_contacter.sql
@@ -1,4 +1,5 @@
 -- 由于数据库中表多，字段分散，所以想形成［注册人姓名，注册人手机号，报名人姓名，报名人手机号］的表，用于信息员进行VIP注册
+-- 要注意orders.'no'这里，单引号可能有问题
 SELECT A.id, A.realname, A.mobile, B.id, B.realname, B.mobile
 FROM (
         SELECT users_contacter.id, users_contacter.user_id, users_contacter.realname, users_contacter.mobile
